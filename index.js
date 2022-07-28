51c42a0b414159a46eacc4bfd40663864124e49c
Lucas Dellamaggiore <88515730+LucasDellamaggiore0@users.noreply.github.com>
2022-07-28 18:03:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const {PORT} = process.env;
 
 
 sequelize.sync({force: false}).then(() => {
-    server.listen(process.env.PORT || 3001, () => {
+    server.listen(process.env.PORT, () => {
         console.log(`Server listening on port ${process.env.PORT}`);
         // console.log(sequelize.models);
     })
